6966f601e2a290289fcafba1f16e5492b2df7de7
spacefall <91277246+spacefall@users.noreply.github.com>
2023-05-26 12:32:54 +0200

    Test touch

--- canvas.js
+++ canvas.js
@@ -11,6 +11,38 @@ let coord = { x: 0, y: 0 };
 // tiene le coordinate della linea del canvas
 const linecoords = [];
 
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
 // Listener per il mouse o quando viene ridimensionata la finestra
 canvas.addEventListener("mousedown", start);
 canvas.addEventListener("mouseup", stop);
