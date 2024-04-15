910211910857801133df22e677d2b7d72a506a48
Luca5822_ <83072262+Luca5822@users.noreply.github.com>
2024-04-15 09:15:19 +0000

    Revert "colori 2"
    This reverts commit b9875d5f7c00b235512a33497547807ed37614bb.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let giocatore;
 const colors = document.querySelector(":root");
 
 // colori dei giocatori
-let coloriGiocatori = ["#e1c9c7", "#260022"];
+let coloriGiocatori = ["#7da891", "#3c3a7d"];
 
 // array che indica tutte le stecchette disponibili
 let stecchetteLibere = [];
